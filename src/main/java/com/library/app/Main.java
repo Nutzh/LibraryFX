727f7dbb0e39c7b8cee19aaba0279e596faae768
0xDEZZNUTZ <youssef17704@gmail.com>
2025-12-24 23:33:56 +0100

    initialisation de projet POO LibraryFX

---
+++ src/main/java/com/library/app/Main.java
@@ -0,0 +1,23 @@
+package com.library.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+
+        stage.setTitle("Library Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/library/app/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.library.app.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    public void onAddBookClicked() {
+        System.out.println("Add Book Clicked");
+    }
+}
