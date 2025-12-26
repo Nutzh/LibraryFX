906ee055b5f9162faa65be458b4696c559fd9117
0xDEZZNUTZ <youssef17704@gmail.com>
2025-12-26 23:44:23 +0100

    ajouter exception et fichier DateUtils et verification des chaines

---
+++ src/main/java/com/library/app/exception/LimiteEmprunteException.java
@@ -0,0 +1,9 @@
+package com.library.app.exception;
+
+public class LimiteEmprunteException  extends Exception {
+    
+    public LimiteEmprunteException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/main/java/com/library/app/exception/LivreIndisponible.java
@@ -0,0 +1,9 @@
+package com.library.app.exception;
+
+public class LivreIndisponible extends Exception {
+    
+    public LivreIndisponible(String message) {
+        super(message);
+    }
+    
+}
