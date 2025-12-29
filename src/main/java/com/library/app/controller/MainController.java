6569ba14e62df826179e38f05ea0f5fe64dcb507
0xDEZZNUTZ <youssef17704@gmail.com>
2025-12-29 08:35:57 +0100

    most of myPart (controller,View)

--- src/main/java/com/library/app/controller/MainController.java
+++ src/main/java/com/library/app/controller/MainController.java
@@ -1,11 +1,39 @@
 package com.library.app.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
+
+import java.io.IOException;
 
 public class MainController {
 
     @FXML
-    public void onAddBookClicked() {
-        System.out.println("Add Book Clicked");
+    private StackPane contentArea;
+
+    private void loadView(String viewName) {
+        try {
+            Parent view = FXMLLoader.load(getClass().getResource("/com/library/app/" + viewName));
+            contentArea.getChildren().setAll(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Menu actions 
+    @FXML
+    public void showLivres() {
+        loadView("livre_view.fxml");     // Brahim
+    }
+
+    @FXML
+    public void showMembres() {
+        loadView("membre_view.fxml");    // Hatim
+    }
+
+    @FXML
+    public void showEmprunts() {
+        loadView("emprunt_view.fxml");   // Hakim
     }
 }
