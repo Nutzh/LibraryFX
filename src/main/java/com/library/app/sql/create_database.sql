5004b94edff4ceaa41d2c345661d0c9faee50d9d
BrahimMekkaoui <brahim.mekkaoui@esi.ac.ma>
2025-12-29 13:55:07 +0100

    structure de Livre

--- src/main/java/com/library/app/sql/create_database.sql
+++ src/main/java/com/library/app/sql/create_database.sql
@@ -2,9 +2,13 @@ CREATE DATABASE IF NOT EXISTS library_db;
 USE library_db;
 
 /* TABLE LIVRE — brahim */
-CREATE TABLE livre (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    
+CREATE TABLE IF NOT EXISTS livres (
+    id VARCHAR(50) PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    auteur VARCHAR(255) NOT NULL,
+    annee_publication INT,
+    isbn VARCHAR(20) UNIQUE NOT NULL,
+    disponible BOOLEAN DEFAULT TRUE
 );
 
 /* TABLE MEMBRE — hatim */
