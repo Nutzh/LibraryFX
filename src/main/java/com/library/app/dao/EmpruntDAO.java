1ad29b2e56cff373a45fc8cac20dc9c8300506f6
Abdelhakim Benatti <abdelhakimb404@gmail.com>
2025-12-29 14:44:31 +0100

    Correction

--- src/main/java/com/library/app/dao/EmpruntDAO.java
+++ src/main/java/com/library/app/dao/EmpruntDAO.java
@@ -1,5 +1,15 @@
 package com.library.app.dao;
 
-public class EmpruntDAO {
-    
+import com.library.app.model.Emprunt;
+import java.util.List;
+
+public interface EmpruntDAO {
+
+    void save(Emprunt emprunt);
+    Emprunt findById(int id);
+    List<Emprunt> findAll();
+    List<Emprunt> findByMembre(int membreId);
+    List<Emprunt> findEnCours();
+    int countEmpruntsEnCours(int membreId);
+    void update(Emprunt emprunt);
 }
